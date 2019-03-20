bc53d75b4a8486a1ae41f04b8336f272d971439b
cagdasmer <43043172+cagdasmer@users.noreply.github.com>
2019-03-20 22:53:52 +0300

    Multi reducer & assignment

---
+++ _redux-start_multi-reducer/src/store/reducers/counter.js
@@ -0,0 +1,37 @@
+import * as actions from '../actions';
+
+const initialState = {
+    counter: 0,
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case actions.INCREMENT: 
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+       
+        case actions.DECREMENT: 
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+
+       case actions.ADD: 
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+
+        case actions.SUBSTRACT: 
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+    }
+
+    return state;
+}
+
+export default reducer;
