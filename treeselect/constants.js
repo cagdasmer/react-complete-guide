c8fda731b7e3831fe7d83169d8ab1ff424d2fb74
cagdasmer <43043172+cagdasmer@users.noreply.github.com>
2019-04-07 20:16:52 +0300

    Create constants.js

---
+++ treeselect/constants.js
@@ -0,0 +1,125 @@
+export const treeData = [
+    {
+      title: 'A',
+      value: 'a',
+      key: 'a',
+      children: [
+        {
+          title: 'D',
+          value: 'a-d',
+          key: 'a-d',
+          children:[
+            {
+              title: 'D_1',
+              value: 'a-d-d_1',
+              key: 'a-d-d_1',
+            },
+            {
+              title: 'D_2',
+              value: 'a-d-d_2',
+              key: 'a-d-d_2',
+            }
+          ]
+        },
+        {
+          title: 'E',
+          value: 'a-e',
+          key: 'a-e',
+          children: [
+            {
+              title: 'E_1',
+              value: 'a-e-e_1',
+              key: 'a-e-e_1',
+            },
+            {
+              title: 'E_2',
+              value: 'a-e-e_2',
+              key: 'a-e-e_2',
+            }
+          ]
+        }
+      ],
+    }, 
+    {
+      title: 'B',
+      value: 'b',
+      key: 'b',
+      children: [
+        {
+          title: 'D',
+          value: 'b-d',
+          key: 'b-d',
+          children:[
+            {
+              title: 'D_1',
+              value: 'b-d-d_1',
+              key: 'b-d-d_1',
+            },
+            {
+              title: 'D_2',
+              value: 'b-d-d_2',
+              key: 'b-d-d_2',
+            }
+          ]
+        },
+        {
+          title: 'E',
+          value: 'b-e',
+          key: 'b-e',
+          children: [
+            {
+              title: 'E_1',
+              value: 'b-e-e_1',
+              key: 'b-e-e_1',
+            },
+            {
+              title: 'E_2',
+              value: 'b-e-e_2',
+              key: 'b-e-e_2',
+            }
+          ]
+        }
+      ],
+    },
+    {
+      title: 'C',
+      value: 'c',
+      key: 'c',
+      children: [
+        {
+          title: 'F',
+          value: 'c-f',
+          key: 'c-f',
+          children:[
+            {
+              title: 'F_1',
+              value: 'c-f-f_1',
+              key: 'c-f-f_1'
+            },
+            {
+              title: 'F_2',
+              value: 'c-f-f_2',
+              key: 'c-f-f_2'
+            }
+          ]
+        }, 
+        {
+          title: 'G',
+          value: 'c-g',
+          key: 'c-g',
+          children: [
+            {
+              title: 'G_1',
+              value: 'c-g-g_1',
+              key: 'c-g-g_1'
+            },
+            {
+              title: 'G_2',
+              value: 'c-g-g_2',
+              key: 'c-g-g_2'
+            }
+          ]
+        }
+      ],
+    }
+  ];
